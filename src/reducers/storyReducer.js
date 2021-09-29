4ce3ec4d75a50618944276b6c6c0c3fcb35825c0
Bob <OrphaRinkelyaa18@gmail.com>
2021-09-29 12:22:25 +0330

    redux corrected-pizza2

--- src/reducers/storyReducer.js
+++ src/reducers/storyReducer.js
@@ -2,7 +2,7 @@ import {SET_STORY_WRITE} from '../constants/actionTypesConstant'
 const initialState={}
 export default function storyReducer(state= initialState,action){
 	switch (action.type){
-		case "SET_STORY_WRITE":
+		case SET_STORY_WRITE:
 			return{
 				...state,
 				...action.payload
