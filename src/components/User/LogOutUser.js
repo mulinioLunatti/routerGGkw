c16f9ac163d864b4c7b041151cfe5a3a599f067b
Bob <OrphaRinkelyaa18@gmail.com>
2021-10-02 15:34:37 +0330

    fe2-33

--- src/components/User/LogOutUser.js
+++ src/components/User/LogOutUser.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import  { Redirect } from 'react-router-dom'
+import {Redirect, useHistory} from 'react-router-dom'
 import {useUserContext} from "../../contexts/UserContext";
 
 export default function LogoutUser(){
+    const history=useHistory();
     const {user: {current: {userCredential}},logOutUser} = useUserContext();
     if (userCredential) {logOutUser({})}
-    if (!userCredential) return <Redirect to='/'  />;
+    if (!userCredential) history.replace("/");
+    // if (!userCredential) return <Redirect to='/'  />;
     return <React.Fragment></React.Fragment>
 }
